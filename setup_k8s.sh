c499d0b9fbdb6141a1192cf8eeb4ab4a5163557d
Killian - Aidalinfo <146849979+Killian-Aidalinfo@users.noreply.github.com>
2023-12-04 16:36:19 +0100

    Update setup_k8s.sh

--- setup_k8s.sh
+++ setup_k8s.sh
@@ -54,7 +54,7 @@ sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg gnupg
 sudo mkdir -m 755 /etc/apt/keyrings
 # Ajout du dépôt Kubernetes et installation des paquets
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -O https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
